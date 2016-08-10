8da8af57fcf2cdff58de2b07cf36645f627bad57
lenymo <lenymovo@mail.ru>
2016-08-10 18:10:18 +0300

    Версия Кати Поршневой

---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+'use strict';
+
+var gulp = require('gulp'),
+    prefixer = require('gulp-autoprefixer'),
+    watch = require('gulp-watch'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-uglifycss'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('gulp-pngmin'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        html: '*.html',
+        js: 'js/main.js',
+        style: 'style/main.scss',
+        img: 'img/**/*.*',
+        fonts: 'fonts/**/*.*'
+    },
+    watch: {
+        html: '*.html',
+        js: 'js/**/*.js',
+        style: 'style/**/*.scss',
+        img: 'img/**/*.*',
+        fonts: 'fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) 
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) 
+        .pipe(rigger()) 
+        .pipe(sourcemaps.init()) 
+        .pipe(uglify()) 
+        .pipe(sourcemaps.write()) 
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style) 
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) 
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'style:build',
+    'fonts:build',
+    'image:build'
+]);
+
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+
+gulp.task('default', ['build', 'webserver', 'watch']);
---
+++ js/contacts.js
@@ -0,0 +1,77 @@
+var contacts = [
+  {
+    id: 1,
+    name: "ivan",
+    prof: 'ingener',
+    image: "img/1.jpg"
+  }, {
+    id: 2,
+    name: 'vasya',
+    prof: 'programmer',
+    image: "img/2.jpg"
+  }, {
+    id: 3,
+    name: 'petya',
+    prof: 'director',
+    image: "img/3.jpg"
+  }, {
+    id: 4,
+    name: 'ilya',
+    prof: 'manager',
+    image: "img/4.jpg"
+  }
+];
+
+var Contact = React.createClass({
+  render: function(){
+    return (
+      <li>
+        <img src={this.props.img} /* */
+        {this.props.name} /* */
+        {this.props.prof}
+      </li>
+    )
+  }
+});
+
+  var ContactList = React.createClass({
+    getInitialState: function(){
+      return{
+        currArray: contacts
+      }
+    },
+
+    handleEvent1: function(event){
+      var currArray = contacts.filter(function(el){
+        return el.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1;
+      });
+
+      this.setState({
+        currArray: currArray
+      })
+    },
+
+    render: function(){
+      return (
+        <div>
+          <input type="text" onChange={this.handleEvent1}/>
+          <ul>
+            {
+              this.state.currArray.map(function(el){
+                return <Contact 
+                  key={el.id}
+                  name={el.name}
+                  prof={el.prof}
+                  img={el.image}
+                />
+              })
+            }
+          </ul>
+        </div>)
+    }
+  });
+
+  ReactDOM.render(
+    <ContactList />,
+    document.getElementById('content')
+  );
